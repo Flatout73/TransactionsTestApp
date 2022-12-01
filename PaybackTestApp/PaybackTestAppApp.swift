a415f626c48faf001001a57eb046efebb7ea6f04
Leonid Liadveikin <fantom2259@gmail.com>
2022-12-01 21:15:18 +0100

    Initial Commit

---
+++ PaybackTestApp/PaybackTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaybackTestAppApp.swift
+//  PaybackTestApp
+//
+//  Created by Leonid Lyadveykin on 01.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct PaybackTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
